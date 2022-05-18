af246851f71e790693e69dc94b0b57e5d665f2b0
Weijia <zhengwj.william@gmail.com>
2022-05-18 15:18:11 +0800

    with arima

--- time_series/testing.py
+++ time_series/testing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from darts import TimeSeries
 import numpy as np
+import torch
 
 # Read a pandas DataFrame
 df = pd.read_csv('AirPassengers.csv', delimiter=",")
@@ -9,17 +10,35 @@ df = pd.read_csv('AirPassengers.csv', delimiter=",")
 # series = TimeSeries.from_dataframe(df, 'Month', '#Passengers')
 
 
-series =  np.arange(0, 600, step = 2)
+series = TimeSeries.from_values( np.array( np.arange(0, 600, step = 1)) )
+print(series)
 # Set aside the last 36 months as a validation series
 train, val = series[:-36], series[-36:]
 
 from darts.models import ExponentialSmoothing
 from darts.models import ARIMA
+from darts.models import RNNModel
 
 model = ExponentialSmoothing()
-model2 = ARIMA()
+model2 = ARIMA(p = 4, d = 2, q= 1)
+# model3 = RNNModel(
+#     model="LSTM",
+#     hidden_dim=20,
+#     dropout=0,
+#     batch_size=16,
+#     n_epochs=300,
+#     optimizer_kwargs={"lr": 1e-3},
+#     model_name="Air_RNN",
+#     log_tensorboard=True,
+#     random_state=42,
+#     training_length=20,
+#     input_chunk_length=14,
+#     force_reset=True,
+#     save_checkpoints=True,
+# )
+
 model2.fit(train)
-prediction = model2.predict(len(val), num_samples=1000)
+prediction = model2.predict(len(val), num_samples=10)
 
 import matplotlib.pyplot as plt
 
