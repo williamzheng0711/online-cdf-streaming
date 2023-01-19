3049eb9f9ee90f127de089661870464a0f0c45b4
Weijia <zhengwj.william@gmail.com>
2023-01-20 03:15:04 +0800

    明天寫PPT

--- calculate_CI.py
+++ calculate_CI.py
@@ -4,6 +4,6 @@ from scipy.optimize import fsolve
 def full_function(alpha,N,pvalue,qvalue):
     return(beta.ppf(pvalue,alpha*N+1,(1-alpha)*N+1)-qvalue)
 
-for N in [18000]:
+for N in [17500]:
     for pvalue in [.025,.975]:
         print(fsolve(full_function,.5,args=(N,pvalue,.05))[0])
