d90acf663ab3ace17a3b19863dd2ccc323716895
Weijia <zhengwj.william@gmail.com>
2023-06-23 01:15:55 +0800

    deleted useless comments

--- runner.py
+++ runner.py
@@ -21,7 +21,7 @@ parser.add_option("--args", type="string", dest="args", help="Arguments", defaul
 parser.add_option("--algo", type="string", dest="algo", help="Which method to run? Choose ONE from 'OnCDF', 'OnRLS'.", default="OnCDF")
 parser.add_option("--epsilon", type="float", dest="epsilon", help="Target frame loss rate (only useful for OnCDF and OnRLS)", default=0.05)
 parser.add_option("--traceData", type="int", dest="traceData", help="Which trace data to simulate with? Input a number between 1 to 18", default=-1)
-parser.add_option("--trainTime", type="int", dest="trainTime", help="How long (in seconds) is the training time interval in trace data?", default=1500)
+parser.add_option("--trainTime", type="int", dest="trainTime", help="How long (in seconds) is the training time interval in trace data?", default=150)
 parser.add_option("--testTime", type="int", dest="testTime", help="How long (in seconds) is the testing time interval in trace data?", default=1000)
 parser.add_option("--Tb", type="float", dest="pBufferTime", help="player prefetch", default = -1 )
 (options, args) = parser.parse_args()
@@ -29,20 +29,19 @@ algo = options.algo
 assert algo in ["OnCDF", "OnRLS"]
 traceData = options.traceData
 assert traceData in range(19)
-trainTime = options.trainTime                                      # number of active users
+trainTime = options.trainTime                                     
 assert trainTime > 100 
-testTime = options.testTime                                      # number of active users
+testTime = options.testTime                                     
 assert testTime > 0
 epsilon = options.epsilon
 assert epsilon > 0 and epsilon < 1
 pBufferTime = options.pBufferTime
 assert pBufferTime > 0
 
-print("Algo: " + algo +" Trace No.: " + str(traceData) +" trainTime= " + str(trainTime) + " testTime= "  + str(testTime) + " epsilon= "+ str(epsilon) + " Tb=" + str(pBufferTime))
+print("Algo: " + algo +", Trace No.: " + str(traceData) +", trainTime= " + str(trainTime) + ", testTime= "  + str(testTime) + ", epsilon= "+ str(epsilon) + ", Tb=" + str(pBufferTime))
 
 ### Read in the trace data. 
 traceDir = './dataset/trace_data_simulator/'
-# print("現在用的是dataset " + str(traceData) + " pBufferTime=" + str(pBufferTime))
 count = 0
 initialTime = 0
 networkEnvTime = [] 
@@ -78,7 +77,6 @@ for _ in range( fullTimeInSec * FPS ):
 
 # variables that would be re-initialized every 100 frames
 countExceed = 0                 # Integer. No. of exceeds in the this 100 frames
-
 # variables that would get updated for all simulation
 runningTime = 0                 # Float. The simulation clock, in seconds.
 uploadDuration = 0              # Float. A variable to store the transmission time of frames.
@@ -194,38 +192,22 @@ for singleFrame in tqdm(range( fullTimeInSec * FPS )) if debug==False else range
 
             percentiles.append( np.count_nonzero(decision_list <= log_maxData) / len(decision_list) )
             percentiles = percentiles[max(len(percentiles)-backLen, 0) : ]
-        
-        # else: 
-        #     if now_go_real: 
-        #         print(singleFrame, "WHAT??", len(lookbackwardHistogramS))
-        #         break
 
 
     elif (algo == "OnRLS"):
         arr1 = np.array(np.array(realVideoFrameSize[-5:])/np.array(transmitHistoryTimeLog[-5:])) if (len(realVideoFrameSize)>=5 and len(transmitHistoryTimeLog)>=5) else np.ones(5) 
-        # arr2 = np.array(realVideoFrameSize[-5:]) if len(realVideoFrameSize)>=5 else np.ones(5)
-        # arr3 = np.array(transmitHistoryTimeLog[-5:]) if len(transmitHistoryTimeLog)>=5 else np.ones(5) 
-        # arr3 = np.append(arr3, timeSlot)
-        # input = np.concatenate((arr1, arr3))
         input = arr1
-        # print(input)
         updatedX = input
                    
         c_avg_new_hat = filt.predict(updatedX) 
         pass
         error_quantile = np.quantile(errors[-1000:], epsilon) if len(errors)>0 else 0
         r_k = c_avg_new_hat + error_quantile
-        # r_k = c_avg_new_hat
 
         credibleRegion90Len = timeSlot*(np.quantile(errors[-1000:], 0.95) - np.quantile(errors[-1000:], 0.05)) if len(errors)>0 else 0
         
         credibleLens.append(credibleRegion90Len)
         suggestedFrameSize = timeSlot * r_k
-        # maxData = utils.calMaxData(prevTime=runningTime, 
-        #                             laterTime=runningTime+timeSlot, 
-        #                             packet_level_timestamp= networkEnvTime,
-        #                             packet_level_data= networkEnvPacket,)
-
 
     if suggestedFrameSize == -np.Infinity:
         suggestedFrameSize = c_hat_init * timeSlot
@@ -280,42 +262,17 @@ for singleFrame in tqdm(range( fullTimeInSec * FPS )) if debug==False else range
     ########################################################################################
     # will go to next "singleFrame"
 
-# pyplot.hist(errors,bins=1000, range=(-50,50))
-# pyplot.show()
+
 per100lr = exceedsRatios[1:]
 maxThroughputAll =  utils.calMaxData(startingFrame*(1/FPS), runningTime, networkEnvTime, networkEnvPacket)
 
 JBurn = int(len(credibleLens)/2)
-# pyplot.plot(credibleLens[JBurn:])
-# pyplot.show()
+
 
 print( "Mean of credible lengths: " + str(np.mean(credibleLens[JBurn:])))
 print( "Mean throughput in Mbps: " + str( videoCumsize/(runningTime- startingFrame/FPS) ))
 print( "Avg. Bandwidth in Mbps: " + str( maxThroughputAll/(runningTime- startingFrame/FPS) ))
 print( "Mean of per100lr: " + str( np.mean(per100lr)) )
 print( "Average delay time (about non-defective): " + str( np.mean(delays) ) + " seconds" )
-
-# pyplot.title("Dataset " + str(traceData))
-# pyplot.plot(delays, color="blue")
-# pyplot.axhline(pBufferTime, color="red")
-# pyplot.legend(["real delay", "maxBuffer"])
-# pyplot.show()
-
-# pyplot.title("Dataset " + str(traceData))
-# pyplot.plot(per100lr, color="blue")
-# pyplot.xlabel("100 frames per slot")
-# pyplot.ylabel("loss rate of that 100 frames")   
-# pyplot.axhline(epsilon, color=str(epsilon))
-# pyplot.legend(["real loss rate", "target 0.05"])
-# pyplot.show()
-
-# pyplot.hist(percentiles, bins=50, cumulative=True, density=True)
-# pyplot.axline((0, 0), slope=1)
-# pyplot.show()
-
-
-
-
-
 
 print("Done!")
